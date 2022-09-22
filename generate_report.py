e8cadd9e41e04a4f6f1e1e30db60394ad8d59c10
Cheng-Yu (Benjamin) Chiang <cchiang@ucsd.edu>
2022-09-22 16:58:55 -0700

    added parsing summary and tracker csv function

--- generate_report.py
+++ generate_report.py
@@ -1,34 +1,86 @@
-from itertools import cycle
-from jinja2 import Environment, FileSystemLoader
+# built in python modules
+import logging
 import datetime as dt
+from pathlib import Path
 
-cycle_counts = [1119, 1425]
-# for pack_name in ('NP5', 'NP6'):
-#     cycle_count = input(f'Enter cycle count for {pack_name}:')
-#     cycle_counts.append(cycle_count)
-
-
-# 2. Create a template Environment
-env = Environment(loader=FileSystemLoader('templates'))
-
-# 3. Load the template from the Environment
-template = env.get_template('./index.html')
+# 3rd party modules
+import pandas as pd
+import numpy as np
+from jinja2 import Environment, FileSystemLoader
 
 
-date_test = [{'pack_num': 5, 'test_name': 'Aging 61', 'start': dt.datetime.now().date(), 'end': dt.datetime.now().date()},
-             {'pack_num': 6, 'test_name': 'Aging 32', 'start': dt.datetime.now().date(), 'end': '2022-12-23'},]
+def get_test_metadata(fp: Path) -> tuple:
+    """Extracts the test metadata from the file name"""
+    df = pd.read_csv(fp)
+    df.dropna(inplace=True, subset=['Cycle']) # drop empty rows
+    last_row = df.iloc[-1, :]
 
-# 4. Render the template with variables
-html = template.render(page_title_text='Report',
-                       date=dt.datetime.now().date(),
-                       title_text='Nissan Cycle Aging Report',
-                       name='Ben',
-                       date_written=str(dt.datetime.now().date()),
-                       tests=date_test,
-                       np5_cycle_count=cycle_counts[0],
-                       np6_cycle_count=cycle_counts[1],
-                       )
+    if last_row[:3].hasnans: # if any of the first 3 columns are NaN
+        # if so, push back one test
+        last_row = df.iloc[-2, :3]
+    
+    # extract needed data for the report
+    test_name = last_row['Cycle']
+    start_date = last_row['Date Started']
+    end_date = last_row['Date Completed']
+    return (test_name, start_date, end_date)
 
-# 5. Write the template to an HTML file
-with open('generated_report.html', 'w') as f:
-    f.write(html)
+
+def calc_cycles(df):
+    """Calculate the number of cycles that the battery has gone through"""
+    rated_cap = 56.3 # rated capacity of the Nissan batteries 
+    rows = df.shape[0]
+    dch1 = df['DCH1']
+    dch2 = df['DCH2']
+
+    cycles = np.zeros((rows,))
+    cycles[0] = dch1[0]
+    for i in range(rows-1):
+        cycles[i+1] = cycles[i] + dch1[i+1] + dch2[i]
+    return cycles / rated_cap
+
+
+def main():
+    """Main function"""
+    # set logging level and formatting
+    logging.basicConfig(format='%(asctime)s | %(levelname)s: %(message)s', level=logging.INFO)
+
+    # extract cycle count from summmary csvs
+    cycle_counts = []
+    summary_data_root = Path('./input/summary_data/')
+    for fp in summary_data_root.glob('*.csv'):
+        logging.info(f'Calculating cycle count from {fp.name}')
+        df = pd.read_csv(fp)
+        cycle_counts.append(round(calc_cycles(df)[-1]))
+
+    # jinja2 setup
+    env = Environment(loader=FileSystemLoader('templates'))
+    template = env.get_template('./index.html')
+
+    timeline_table = dict()
+    progress_tracker_root = Path('./input/progress_trackers/')
+    for fp in progress_tracker_root.glob('*.csv'):
+        logging.info(f'Generating timeline table data from {fp.name}')
+        pack_num = fp.name.split(" ")[6]
+        timeline_table[pack_num] = get_test_metadata(fp)
+
+    timeline_table = dict(sorted(timeline_table.items())) # sort dict by key value
+
+    # render the template with variables
+    html = template.render(page_title_text='Report',
+                        date=dt.datetime.now().date(),
+                        title_text='Nissan Cycle Aging Report',
+                        name='Ben',
+                        date_written=str(dt.datetime.now().date()),
+                        timeline_table=timeline_table,
+                        np5_cycle_count=cycle_counts[0],
+                        np6_cycle_count=cycle_counts[1],
+                        )
+
+    # write the template to an HTML file
+    with open('generated_report.html', 'w') as f:
+        f.write(html)
+
+if __name__ == '__main__':
+    logging.info('Starting report generation')
+    main()
--- rename_plots.py
+++ rename_plots.py
@@ -1,13 +1,16 @@
 from pathlib import Path 
 import re
+import logging
 
 if __name__ == '__main__':
-    print("\nRenaming plots...")
-    root = Path('./input') # set where to look for image
+    logging.basicConfig(format='%(asctime)s | %(levelname)s: %(message)s', level=logging.INFO) # set logging level and formatting
+    
+    root = Path('./input/images') # set where to look for image
     dest = Path('./assets/img')
-    files = root.glob('*.jpg') # get all jpg files in plots folder
 
-    # rename files
+    logging.info(f'Starting rename of images in {root} and moving to {dest}')
+
+    files = root.glob('*.jpg') # get all jpg files in plots folder
     for file in files:
         # if no npx in name, skip
         try: 
@@ -16,9 +19,9 @@ if __name__ == '__main__':
             # check if its gantt chart
             if file.name == 'Nissan Gantt Chart.jpg':
                 gantt_dest = dest / 'gantt.jpg'
-                print(f'Moving {file} to {gantt_dest}')
+                logging.info(f'Moving {file} to {gantt_dest}')
                 file.rename(gantt_dest)
-            print("No NP name found")
+            
             continue
 
         if file.name.find('SOH') > 0:
@@ -27,7 +30,8 @@ if __name__ == '__main__':
             plot_type = 'IR'
 
         summary_plot_dest = dest / f'{NPname}_{plot_type}.jpg'
-        print(f'Moving {file} to {summary_plot_dest}')
+        logging.info(f'Moving {file} to {summary_plot_dest}')
         file.rename(summary_plot_dest)
-    print("Done\n")
+    
+    logging.info('Finished renaming images')
         
