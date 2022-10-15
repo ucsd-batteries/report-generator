cb0e2a8f338b544a88e5bbe5f203aa94e63f26c7
Cheng-Yu (Benjamin) Chiang <cchiang@ucsd.edu>
2022-10-15 15:04:00 -0700

    oop-ify battery

--- generate_report.py
+++ generate_report.py
@@ -7,37 +7,75 @@ from pathlib import Path
 import pandas as pd
 import numpy as np
 from jinja2 import Environment, FileSystemLoader
+from utils.utils import bcolors
 
 
-def get_test_metadata(fp: Path) -> tuple:
-    """Extracts the test metadata from the file name"""
-    df = pd.read_csv(fp)
-    df.dropna(inplace=True, subset=['Cycle']) # drop empty rows
-    last_row = df.iloc[-1, :]
 
-    if last_row[:3].hasnans: # if any of the first 3 columns are NaN
-        # if so, push back one test
-        last_row = df.iloc[-2, :3]
-    
-    # extract needed data for the report
-    test_name = last_row['Cycle']
-    start_date = last_row['Date Started']
-    end_date = last_row['Date Completed']
-    return (test_name, start_date, end_date)
+class Battery:
+    """Class used to represent a battery"""
+    def __init__(self, pack_num:int, cell_num:int, rated_cap:float, summary_fp:str, metadata_fp:str=None) -> None:
+        try:
+            self.summary_df = pd.read_csv(summary_fp)
+        except FileNotFoundError:
+            logging.error(f"File {summary_fp} not found")
+            raise FileNotFoundError
+        self.packnum = pack_num
+        self.cell_num = cell_num
+        self._rated_cap = rated_cap # rated capacity of the Nissan batteries
+        self.metadata_df = pd.read_csv(metadata_fp) if metadata_fp else None
+        self._cycles_record = self._calc_cycles()
+        self._total_cycles = self.cycles_record[-1]
+        self._last_test_name, self._start_date, self._end_date = self._get_test_metadata()
 
+    def _calc_cycles(self) -> np.ndarray:
+        """Calculate the number of cycles at the end of each test"""
+        rows = self.summary_df.shape[0]
+        dch1 = self.summary_df['DCH1']
+        dch2 = self.summary_df['DCH2']
 
-def calc_cycles(df):
-    """Calculate the number of cycles that the battery has gone through"""
-    rated_cap = 56.3 # rated capacity of the Nissan batteries 
-    rows = df.shape[0]
-    dch1 = df['DCH1']
-    dch2 = df['DCH2']
+        cycles = np.zeros((rows,))
+        cycles[0] = dch1[0]
+        for i in range(rows-1):
+            cycles[i+1] = cycles[i] + dch1[i+1] + dch2[i]
+        return cycles / self.rated_cap
 
-    cycles = np.zeros((rows,))
-    cycles[0] = dch1[0]
-    for i in range(rows-1):
-        cycles[i+1] = cycles[i] + dch1[i+1] + dch2[i]
-    return cycles / rated_cap
+    def _get_test_metadata(self) -> tuple:
+        """Extracts the test metadata from the progress tracker csv"""
+        self.metadata_df.dropna(inplace=True, subset=['Cycle']) # drop empty rows
+        last_row = self.metadata_df.iloc[-1, :]
+
+        if last_row[:3].hasnans: # if any of the first 3 columns are NaN
+            last_row = self.metadata_df.iloc[-2, :3] # if so, push back one test
+        
+        # extract data for the report
+        test_name = last_row['Cycle']
+        start_date = last_row['Date Started']
+        end_date = last_row['Date Completed']
+        return (test_name, start_date, end_date)
+
+    @property
+    def total_cycles(self):
+        return self._total_cycles
+
+    @property
+    def rated_cap(self):
+        return self._rated_cap
+
+    @property
+    def cycles_record(self):
+        return self._cycles_record
+
+    @property
+    def last_test_name(self):
+        return self._last_test_name
+
+    @property
+    def start_date(self):
+        return self._start_date
+
+    @property
+    def end_date(self):
+        return self._end_date
 
 
 def write_html(html: str, fp: str, mode: str = 'w') -> None:
@@ -46,56 +84,59 @@ def write_html(html: str, fp: str, mode: str = 'w') -> None:
         f.write(html)
 
 
-def backup(fp: Path) -> None:
-    """Backs up the file by renaming it with a '.back' suffix"""
-    backup_fp = fp.with_suffix('.back.html')
-    fp.rename(backup_fp)
+# TODO fix this
+# def backup(fp: Path) -> None:
+#     """Backs up the file by renaming it with a '.back' suffix"""
+#     backup_fp = fp.with_suffix('.back.html')
+#     if fp.exists():
+#         fp.unlink()
+#     fp.rename(backup_fp)
+
 
 def main():
     """Main function"""
-    # extract cycle count from summmary csvs
-    cycle_counts = []
-    summary_data_root = Path('./input/summary_data/')
-    for fp in summary_data_root.glob('*.csv'):
-        logging.info(f'Calculating cycle count from {fp.name}')
-        df = pd.read_csv(fp)
-        cycle_counts.append(round(calc_cycles(df)[-1]))
+    # instantiate the battery objects, change the filepaths as needed
+    NP_rated_cap = 56.3
+    NP5 = Battery(5, 16, NP_rated_cap, 'input/summary_data/NP5_test_summary.csv', 'input\progress_trackers\Testing Progress Tracker - Nissan Pack 5 (B).csv')
+    NP6 = Battery(6, 16, NP_rated_cap, 'input/summary_data/NP6_test_summary.csv', 'input\progress_trackers\Testing Progress Tracker - Nissan Pack 6 (A).csv')
 
     # jinja2 setup
+    template_html = 'nissan.html' # modify this to change the template
+    logging.info(f"Loading template from {'./templates/' + template_html}")
     env = Environment(loader=FileSystemLoader('templates'))
-    template = env.get_template('./nissan.html')
+    template = env.get_template(template_html)
 
-    timeline_table = dict()
-    progress_tracker_root = Path('./input/progress_trackers/')
-    for fp in progress_tracker_root.glob('*.csv'):
-        logging.info(f'Generating timeline table data from {fp.name}')
-        pack_num = fp.name.split(" ")[6]
-        timeline_table[pack_num] = get_test_metadata(fp)
-
+    # organize timeline data into a dictionary
+    timeline_table = {}
+    timeline_table[NP5.packnum] = NP5.last_test_name, NP5.start_date, NP5.end_date
+    timeline_table[NP6.packnum] = NP6.last_test_name, NP6.start_date, NP6.end_date
     timeline_table = dict(sorted(timeline_table.items())) # sort dict by key value
 
-    # render the template with variables
-    html = template.render(page_title_text='Report',
+    # render the template with variables, modify the variables as needed
+    logging.info("Rendering template")
+    html = template.render(page_title_text='Nissan Report',
                         date=dt.datetime.now().date(),
                         title_text='Nissan Cycle Aging Report',
                         name='Ben',
                         date_written=str(dt.datetime.now().date()),
                         timeline_table=timeline_table,
-                        np5_cycle_count=cycle_counts[0],
-                        np6_cycle_count=cycle_counts[1],
+                        np5_cycle_count=round(NP5.total_cycles),
+                        np6_cycle_count=round(NP6.total_cycles),
                         )
-
 
     # write the template to an HTML file
     generated_html_fp = Path('./nissan.html')
-    if generated_html_fp.exists():
-        backup(generated_html_fp)    
-    write_html(html, 'nissan.html')
+    # TODO: backup the file before writing 
+    logging.info(f"Writing HTML to {generated_html_fp}")
+    write_html(html, generated_html_fp)
 
 if __name__ == '__main__':
     # set logging level and formatting
     logging.basicConfig(format='%(asctime)s | %(levelname)s: %(message)s', level=logging.INFO)
-    if input("Are you sure you want to generate the report? (y/n): ") == 'y':
+    print(f'{bcolors.HEADER}Don\'t forget keep a record of the previous reports!{bcolors.ENDC}')
+    if input(f"{bcolors.WARNING}OK to overwrite to the old report? (y/n): {bcolors.ENDC}") == 'y':
         logging.info('Starting report generation')
         main()
-        logging.info('Saved')
+        logging.info('Done!')
+    else:
+        logging.info('Exiting')
